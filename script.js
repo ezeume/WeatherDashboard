61ae19af8f3e8296a1cc86d45071f3f29bcc2cc0
Nelson Ezeume <nelsonezeume@yahoo.com>
2020-02-19 22:41:27 -0500

    commit uvIndex update

--- script.js
+++ script.js
@@ -23,6 +23,21 @@ $(document).ready(function () {
         var city = $("#city").val();
         var key = "9ac526052b4c1d3800701ee3a4c35a8e";
 
+        ///////////////////
+        //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+        
+        console.log("api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+key)
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+key,
+            method: "GET"
+            
+        }).then(function(fiveday){
+            console.log(fiveday);
+        })
+
+        /////////////
+
+
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/weather",
             method: "GET",
@@ -36,9 +51,23 @@ $(document).ready(function () {
                 $(".temp").text("Temperature (C) " + response.main.temp);
                 $(".humidity").text("Humidity: " + response.main.humidity);
                 $(".wind").text("Wind Speed:" + response.wind.speed);
-                $(".uv").text("UV Index:" + response.main.uvIndex);
+                
+                var lon= response.coord.lon;
+                var lat= response.coord.lat;
+                console.log("http://api.openweathermap.org/data/2.5/uvi?appid="+ key + "&lat="+lat+"&lon="+lon)
+                //http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
 
-                console.log("Temperature(F): " + response.main.temp);
+                $.ajax({
+                    url: "http://api.openweathermap.org/data/2.5/uvi?appid="+ key + "&lat="+lat+"&lon="+lon,
+                    method: "GET"
+                    
+                })
+                .then(function(ajaxrequest2){
+                    //uv vis data
+                    console.log(ajaxrequest2.value)
+                    $(".uv").text("UV Index:" + ajaxrequest2.value);
+                })
+                //console.log("Temperature(F): " + response.main.temp);
 
             })
 
